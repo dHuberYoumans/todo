7ea756f110290c1b5007e67eb001d86e25fed47e
Donald Huber-Youmans <dhuber-youmans@joko.com>
2025-11-24 09:43:30 +0100

    fix: use same table style in list_by_due_date and list_by_tag
    As we have centralized the way we print the table in the list commands,
    we have to update also the list_by_due_date and list_by_tag commands in
    order to ensure a unified UX.
    
    In this commit, we apply the necessary changes.

--- src/commands/list/by_due_date.rs
+++ src/commands/list/by_due_date.rs
@@ -1,7 +1,6 @@
 use anyhow::{anyhow, Result};
-use tabled::settings::{object::Columns, Modify, Style, Width};
 
-use crate::domain::{Datetime, TodoItemRepository, TodoList};
+use crate::domain::{Datetime, TodoItemRepository, TodoList, TodoListTable};
 
 impl TodoList {
     pub fn list_due_date(
@@ -18,16 +17,8 @@ impl TodoList {
         for entry in entries {
             let _ = &self.tasks.push(entry);
         }
-        let mut table = tabled::Table::new(&self.tasks);
-        table
-            .with(Modify::new(Columns::single(0)).with(Width::increase(5))) // id
-            .with(Modify::new(Columns::single(1)).with(Width::wrap(60))) // task
-            .with(Modify::new(Columns::single(2)).with(Width::increase(3))) // status
-            .with(Modify::new(Columns::single(3)).with(Width::increase(3))) // prio
-            .with(Modify::new(Columns::single(4)).with(Width::increase(3))) // due
-            .with(Modify::new(Columns::single(5)).with(Width::wrap(12))) // tag
-            .with(Style::modern_rounded());
-        println!("{}", table);
+        let table = TodoListTable::new(&self.tasks);
+        table.print();
         Ok(())
     }
 }
--- src/commands/list/by_tag.rs
+++ src/commands/list/by_tag.rs
@@ -1,8 +1,6 @@
 use anyhow::Result;
 
-use tabled::settings::{object::Columns, Modify, Style, Width};
-
-use crate::domain::Tag;
+use crate::domain::{Tag, TodoListTable};
 use crate::domain::{TodoItemRepository, TodoList};
 
 impl TodoList {
@@ -12,16 +10,8 @@ impl TodoList {
         for entry in entries {
             let _ = &self.tasks.push(entry);
         }
-        let mut table = tabled::Table::new(&self.tasks);
-        table
-            .with(Modify::new(Columns::single(0)).with(Width::increase(5))) // id
-            .with(Modify::new(Columns::single(1)).with(Width::wrap(60))) // task
-            .with(Modify::new(Columns::single(2)).with(Width::increase(3))) // status
-            .with(Modify::new(Columns::single(3)).with(Width::increase(3))) // prio
-            .with(Modify::new(Columns::single(4)).with(Width::increase(3))) // due
-            .with(Modify::new(Columns::single(5)).with(Width::wrap(12))) // tag
-            .with(Style::modern_rounded());
-        println!("{}", table);
+        let table = TodoListTable::new(&self.tasks);
+        table.print();
         Ok(())
     }
 }
